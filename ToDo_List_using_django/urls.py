96562f8a623fe97eb5dd8bd68b5a940a66a8337f
licofiS <samikchoudhury15@gmail.com>
2021-06-09 23:20:46 +0530

    Project Setup 2nd setup

--- ToDo_List_using_django/urls.py
+++ ToDo_List_using_django/urls.py
@@ -9,13 +9,15 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URL conf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls')),
 ]
