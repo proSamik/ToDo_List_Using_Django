725b3293193275401970137dc9bf7202989e1890
licofiS <samikchoudhury15@gmail.com>
2021-06-11 01:30:28 +0530

    Login page added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,24 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_sucess_url(self):
+        return reverse_lazy('login')
 
 
-def taskist():
-    return HttpResponse('To Do List')
+
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
