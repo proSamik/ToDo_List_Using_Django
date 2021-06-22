6b2a9c2b2a088ca806f21fe60914fabb213da7e8
licofiS <samikchoudhury15@gmail.com>
2021-06-23 02:15:28 +0530

    Functionality of task page is done

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import TaskList, CustomLoginView
+from .views import TaskList, CustomLoginView, TaskDetail, TaskCreate, TaskUpdate, DeleteView
 
 from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('', TaskList.as_view(), name='tasks'),
+    path('', TaskList.as_view(), name='tasks'), path('task/<int:pk>',TaskDetail.as_view(),name='task'),path('task-create/', TaskCreate.as_view(), name='task-create'),path('task-update/<int:pk>',TaskUpdate.as_view(),name='task-update'),path('task-delete/<int:pk>',DeleteView.as_view(),name='task-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Task
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
@@ -22,3 +27,23 @@ class TaskList(LoginRequiredMixin, ListView):
     context_object_name = 'tasks'
 
 
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name= 'task' 
+    template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name= 'task'
+    success_url = reverse_lazy('tasks')
